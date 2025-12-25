98f33905669c228f70a84e93ceab521e82e94021
Sahveli01 <sahveli94@gmail.com>
2025-12-25 19:38:18 +0300

    Fix typescript error: remove invalid chain prop from MiniKitProvider

--- components/MiniKitProvider.tsx
+++ components/MiniKitProvider.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
-import { baseSepolia } from "wagmi/chains";
 
 export default function MiniKitProviderWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <MiniKitProvider chain={baseSepolia}>{children}</MiniKitProvider>;
+  return <MiniKitProvider>{children}</MiniKitProvider>;
 }
 
